50f6368be15b9cd2a85b6a15fd44c2968204a237
Gabriel Porto <gabrielposorio@icloud.com>
2025-05-15 16:51:16 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,29 +1,50 @@
-// API significa Application Programing Interface
-// Tem requisições do tipo: POST, GET, PUT, DELETE
-// CRUD - Create Reed Update Delete
+// API significa Application Programming Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create Read Update Delete
 // Endpoint
 // Middleware
 
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
+// app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.use((request, response, next) => {
+  if (request.method === "GET" && !request.url.startsWith("/api")) {
+    response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+// app.get("*", async (request, response) => {
+//   response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
